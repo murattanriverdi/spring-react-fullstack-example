4a5d5df1ec3e78014a5cd436931e1519e3b69d42
Murat <murat@Murat-MacBook-Pro.local>
2023-09-14 22:01:18 +0300

    new version

--- spring-app/src/main/java/com/murattanriverdi/app/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.murattanriverdi.app.repository;
-
-import com.murattanriverdi.app.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository  extends JpaRepository<User, Long> {
-
-    User findByUsername(String username);
-}
--- spring-app/src/main/java/com/murattanriverdi/app/service/IUserService.java
+++
@@ -1,8 +0,0 @@
-package com.murattanriverdi.app.service;
-
-import com.murattanriverdi.app.dto.request.UserRequestDto;
-
-public interface IUserService {
-
-    public void save(UserRequestDto userDto);
-}
