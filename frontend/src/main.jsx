a92e005e7957b33f139f9fe17135083936c04886
Murat <murat@Murat-MacBook-Pro.local>
2024-02-21 21:25:08 +0300

    add reaxt router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 //import App from './App.jsx'
-import "./styles.scss"
-import { SignUp } from './pages/SignUp/index.jsx'
+import "./styles.scss";
 import "./locales";
+import router from "./router";
+import { RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SignUp />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
